d0460d754f1fee45bee55209ac9ecc1f9c892b85
Miron Tolkanitsa <miron.tolkanitsa@gmail.com>
2025-07-05 16:31:25 +0300

    api add

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -6,15 +6,22 @@ from . import app, db
 from .forms import OpinionForm
 from .models import Opinion
 
+
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+
 @app.route('/')
 def index_view():
-    quantity = Opinion.query.count()
-    if not quantity:
+    opinion = random_opinion()
+    if opinion is None:
         abort(500)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
-    
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
@@ -25,14 +32,15 @@ def add_opinion_view():
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
         opinion = Opinion(
-            title=form.title.data, 
-            text=text, 
+            title=form.title.data,
+            text=text,
             source=form.source.data
         )
         db.session.add(opinion)
         db.session.commit()
         return redirect(url_for('opinion_view', id=opinion.id))
     return render_template('add_opinion.html', form=form)
+
 
 @app.route('/opinions/<int:id>')
 def opinion_view(id):
